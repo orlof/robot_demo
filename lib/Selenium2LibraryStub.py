a0e33db5dc968ca7f9b0267838d57eca1783ab91
Teppo Koskinen <teppo.koskinen@omenia.fi>
2016-05-25 20:23:27 +0300

    simplified selenium stub

--- lib/Selenium2LibraryStub.py
+++ lib/Selenium2LibraryStub.py
@@ -1,49 +1,6 @@
 class Selenium2LibraryStub:
-    def input_text(self, *args):
-        pass
+    def __getattr__(self, name):
+        return self.stub
 
-    def click_button(self, *args):
+    def stub(self, *args):
         pass
-
-    def click_element(self, *args):
-        pass
-
-    def wait_until_element_is_visible(self, *args):
-        pass
-
-    def go_to(self, *args):
-        pass
-
-    def set_selenium_timeout(self, *args):
-        pass
-
-    def set_screenshot_directory(self, *args):
-        pass
-
-    def open_browser(self, *args):
-        pass
-
-    def close_all_browsers(self, *args):
-        pass
-
-    def location_should_contain(self, *args):
-        pass
-
-    def maximize_browser_window(self, *args):
-        pass
-
-    def page_should_contain_element(self, *args):
-        pass
-
-    def delete_all_cookies(self, *args):
-        pass
-
-    def element_should_be_visible(self, *args):
-        pass
-
-    def press_key(self, *args):
-        pass
-
-    def get_text(self, *args):
-        pass
-
